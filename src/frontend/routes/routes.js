5d9eeb3bcf08a4790f7554ba029a2198b7a6e0d2
shrey27 <shreypandey27@gmail.com>
2022-07-31 21:59:01 +0530

    new domain try

--- src/frontend/routes/routes.js
+++ src/frontend/routes/routes.js
@@ -15,7 +15,7 @@ import PrivateRoute from "./PrivateRoute";
 import { Routes, Route } from "react-router-dom";
 import NotFound from "../pages/notfound";
 
-const EXPRESS_SERVER = "/v1";
+const EXPRESS_SERVER = "https://legionathletics.shrey27.repl.co/v1";
 
 //API endpoints
 export const SIGNUPAPI = EXPRESS_SERVER + "/auth/signup";
